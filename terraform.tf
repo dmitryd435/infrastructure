04c45a50b2fd05171ca341ec3f0d7666bbc8fb8b
db <kriptexm@gmail.com>
2024-08-07 08:43:14 +0100

    Has been added branch env/staging

--- terraform.tf
+++ terraform.tf
@@ -19,6 +19,20 @@ resource "github_repository" "api" {
   visibility  = "public"
   description = "Coffee API repository"
 }
+
+resource "github_branch_protection" "env_staging" {
+  repository_id         = github_repository.api.node_id
+  pattern               = "env/staging"
+  enforce_admins        = true
+  required_status_checks {
+    strict   = true
+  }
+  required_pull_request_reviews {
+    dismiss_stale_reviews      = true
+    require_code_owner_reviews = true
+  }
+}
+
 resource "github_branch_protection" "main" {
   repository_id         = github_repository.api.node_id
   pattern               = "main"
